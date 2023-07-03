89797afe78d8d6e199557a0ee4a217a65eceec8d
Amna-Amna <mailtoamna4@gmail.com>
2023-07-04 00:14:24 +0500

    feat: Add functionality to the tab section.

--- script.js
+++ script.js
@@ -29,29 +29,53 @@ document.addEventListener("keydown", function (e) {
     closeModal();
   }
 });
+
 //Button scrolling
 btnScrollTo.addEventListener("click", function (e) {
   const s1coords = section1.getBoundingClientRect();
   console.log(s1coords);
-  console.log(e.target.getBoundingClientRect());
-  console.log("(X/Y) positions = ", window.pageXOffset, window.pageYOffset);
-  console.log(
-    "viewwport height and width",
-    document.documentElement.clientHeight,
-    document.documentElement.clientWidth
-  );
-  scrolling;
-  window.scrollTo(
-    s1coords.left + window.pageXOffset,
-    s1coords.top + window.pageYOffset
-  );
-  window.scrollTo({
-    left: s1coords.left + window.pageXOffset,
-    top: s1coords.top + window.pageYOffset,
-    behavior: "smooth",
-  });
   section1.scrollIntoView({ behavior: "smooth" });
 });
+
+//Event delegation
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
+    console.log(id);
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
+//tabbed component
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
+tabsContainer.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".operations__tab");
+
+  // Guard clause
+  if (!clicked) return;
+
+  // Remove active classes
+  tabs.forEach((t) => t.classList.remove("operations__tab--active"));
+  tabsContent.forEach((c) => c.classList.remove("operations__content--active"));
+
+  // Activate tab
+  clicked.classList.add("operations__tab--active");
+
+  // Activate content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add("operations__content--active");
+});
+
+// tabs.forEach((e) =>
+//   e.addEventListener("click", () => {
+//     console.log("tabs");
+//   })
+// );
+
 /////nav scrolling
 // document.querySelectorAll(".nav__link").forEach(function (el) {
 //   el.addEventListener("click", function (e) {
@@ -62,15 +86,9 @@ btnScrollTo.addEventListener("click", function (e) {
 //   });
 // });
 
-//Event delegation
-document.querySelector(".nav__links").addEventListener("click", function (e) {
-  e.preventDefault();
-  if (e.target.classList.contains("nav__link")) {
-    const id = e.target.getAttribute("href");
-    console.log(id);
-    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
-  }
-});
+// const h1 = document.querySelector("h1");
+// console.log(h1.querySelectorAll(".highlight"));
+// console.log(h1.childNodes);
 
 //////////////////////////////////////////////////////////////////////////////////
 // const header = document.querySelector(".header");
@@ -119,25 +137,25 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
 // const section1 = document.querySelector("#section--1");
 
 // btnScrollTo.addEventListener("click", function (e) {
-// const s1coords = section1.getBoundingClientRect();
+//   const s1coords = section1.getBoundingClientRect();
 // console.log(s1coords);
-// console.log(e.target.getBoundingClientRect());
-// console.log("(X/Y) positions = ", window.pageXOffset, window.pageYOffset);
-// console.log(
-//   "viewwport height and width",
-//   document.documentElement.clientHeight,
-//   document.documentElement.clientWidth
-// );
-//scrolling
-// window.scrollTo(
-//   s1coords.left + window.pageXOffset,
-//   s1coords.top + window.pageYOffset
-// );
-// window.scrollTo({
-//   left: s1coords.left + window.pageXOffset,
-//   top: s1coords.top + window.pageYOffset,
-//   behavior: "smooth",
-// });
+//   console.log(e.target.getBoundingClientRect());
+//   console.log("(X/Y) positions = ", window.pageXOffset, window.pageYOffset);
+//   console.log(
+//     "viewwport height and width",
+//     document.documentElement.clientHeight,
+//     document.documentElement.clientWidth
+//   );
+//   scrolling;
+//   window.scrollTo(
+//     s1coords.left + window.pageXOffset,
+//     s1coords.top + window.pageYOffset
+//   );
+//   window.scrollTo({
+//     left: s1coords.left + window.pageXOffset,
+//     top: s1coords.top + window.pageYOffset,
+//     behavior: "smooth",
+//   });
 //   section1.scrollIntoView({ behavior: "smooth" });
 // });
 
