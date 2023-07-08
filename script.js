379544f971cc2cfc08585e371b366820a54fcb07
Amna-Amna <mailtoamna4@gmail.com>
2023-07-08 19:20:58 +0500

    feat: Reveal elements on scroll using intersection observer API

--- script.js
+++ script.js
@@ -91,6 +91,39 @@ const handleHover = function(e){
 nav.addEventListener('mouseover',handleHover.bind(0.5))
 
 nav.addEventListener('mouseout',handleHover.bind(1))
+
+const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height
+const stickyNav = function(entries){
+  const [entry] = entries
+  if(!entry.isIntersecting)  nav.classList.add('sticky')
+  else nav.classList.remove('sticky')
+}
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root:null,
+  threshold:0,
+  rootMargin:`-${navHeight}px`,
+})
+
+headerObserver.observe(header)
+//Reveal section
+const allSections = document.querySelectorAll('.section')
+const revealSection = function(entries, observer){
+   const [entry] = entries
+   console.log(entry)
+   if(!entry.isIntersecting) return ;
+   entry.target.classList.remove('section--hidden')
+   observer.unobserve(entry.target)
+}
+const sectionObserver = new IntersectionObserver(revealSection,{
+  root:null,
+  threshold:0.15
+})
+allSections.forEach(function(section){
+  sectionObserver.observe(section)
+  section.classList.add('section--hidden')
+})
+//******************************************************************************************* */
 //sticky navigation
 // const initialCoords = section1.getBoundingClientRect()
 // window.addEventListener('scroll', function(){
@@ -112,23 +145,6 @@ nav.addEventListener('mouseout',handleHover.bind(1))
 // }
 // const observer = new IntersectionObserver(obsCallback, obsOption)
 // observer.observe(section1)
-
-const header = document.querySelector('.header');
-const navHeight = nav.getBoundingClientRect().height
-console.log(navHeight)
-const stickyNav = function(entries){
-  const [entry] = entries
-  console.log(entry)
-  if(!entry.isIntersecting)  nav.classList.add('sticky')
-  else nav.classList.remove('sticky')
-}
-const headerObserver = new IntersectionObserver(stickyNav, {
-  root:null,
-  threshold:0,
-  rootMargin:`-${navHeight}px`,
-})
-
-headerObserver.observe(header)
 // tabs.forEach((e) =>
 //   e.addEventListener("click", () => {
 //     console.log("tabs");
